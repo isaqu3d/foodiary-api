3527b96c53a91981b34c3a1949b68b255af75620
Isaque de Sousa <isaque.error404@gmail.com>
2025-07-19 21:20:37 -0300

    refactor: use signAccessTokenFor in SignInController

--- src/controllers/SignInController.ts
+++ src/controllers/SignInController.ts
@@ -1,9 +1,9 @@
 import { compare } from "bcryptjs";
 import { eq } from "drizzle-orm";
-import { sign } from "jsonwebtoken";
 import { z } from "zod";
 import { db } from "../db";
 import { usersTable } from "../db/schema";
+import { signAccessTokenFor } from "../lib/jwt";
 import { HttpRequest, HttpResponse } from "../types/Http";
 import { badRequest, ok, unauthorized } from "../utils/http";
 
@@ -38,9 +38,7 @@ export class SignInController {
       return unauthorized({ error: "Invalid credentials." });
     }
 
-    const accessToken = sign({ sub: user.id }, process.env.JWT_SECRET!, {
-      expiresIn: "3d",
-    });
+    const accessToken = signAccessTokenFor(user.id);
 
     return ok({
       accessToken,

bb55b55ebdeb19665340df7e8c24e4eb52da9e63
Isaque de Sousa <isaque.error404@gmail.com>
2025-07-19 21:52:04 -0300

    refactor: add db and protected http

--- src/controllers/MeController.ts
+++ src/controllers/MeController.ts
@@ -1,10 +1,24 @@
-import { HttpRequest, HttpResponse } from "../types/Http";
+import { eq } from "drizzle-orm";
+import { db } from "../db";
+import { usersTable } from "../db/schema";
+import { HttpResponse, ProtectedHttpRequest } from "../types/Http";
 import { ok } from "../utils/http";
 
 export class MeController {
-  static async handle({ body }: HttpRequest): Promise<HttpResponse> {
-    return ok({
-      user: "ok",
+  static async handle({ userId }: ProtectedHttpRequest): Promise<HttpResponse> {
+    const user = await db.query.usersTable.findFirst({
+      columns: {
+        id: true,
+        email: true,
+        name: true,
+        calories: true,
+        proteins: true,
+        carbohydrates: true,
+        fats: true,
+      },
+      where: eq(usersTable.id, userId),
     });
+
+    return ok({ user });
   }
 }

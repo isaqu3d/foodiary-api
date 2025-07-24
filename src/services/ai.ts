25d955db22194fc0f53381098514e8d870860139
Isaque de Sousa <isaque.error404@gmail.com>
2025-07-23 21:53:59 -0300

    refactor: process to audio

--- src/services/ai.ts
+++ src/services/ai.ts
@@ -2,13 +2,86 @@ import OpenAI, { toFile } from "openai";
 
 const client = new OpenAI();
 
-export async function transcribeAudio(file: Buffer) {
+export async function transcribeAudio(fileBuffer: Buffer) {
   const transcription = await client.audio.transcriptions.create({
     model: "whisper-1",
     language: "pt",
     response_format: "text",
-    file: await toFile(file, "audio.m4a", { type: "audio/m4a" }),
+    file: await toFile(fileBuffer, "audio.m4a", { type: "audio/m4a" }),
   });
 
   return transcription;
+}
+
+type GetMealDetailsFromTextParams = {
+  text: string;
+  createdAt: Date;
+};
+
+export async function getMealDetailsFromText({
+  createdAt,
+  text,
+}: GetMealDetailsFromTextParams) {
+  const response = await client.chat.completions.create({
+    model: "gpt-4.1-mini",
+    messages: [
+      {
+        role: "system",
+        content: `
+          Você é um nutricionista e está atendendo um de seus pacientes. Você deve responder para ele seguindo as instruções a baixo.
+
+          Seu papel é:
+          1. Dar um nome e escolher um emoji para a refeição baseado no horário dela.
+          2. Identificar os alimentos presentes na imagem.
+          3. Estimar, para cada alimento identificado:
+            - Nome do alimento (em português)
+            - Quantidade aproximada (em gramas ou unidades)
+            - Calorias (kcal)
+            - Carboidratos (g)
+            - Proteínas (g)
+            - Gorduras (g)
+
+          Seja direto, objetivo e evite explicações. Apenas retorne os dados em JSON no formato abaixo:
+
+          {
+            "name": "Jantar",
+            "icon": "🍗",
+            "foods": [
+              {
+                "name": "Arroz branco cozido",
+                "quantity": "150g",
+                "calories": 193,
+                "carbohydrates": 42,
+                "proteins": 3.5,
+                "fats": 0.4
+              },
+              {
+                "name": "Peito de frango grelhado",
+                "quantity": "100g",
+                "calories": 165,
+                "carbohydrates": 0,
+                "proteins": 31,
+                "fats": 3.6
+              }
+            ]
+          }
+        `,
+      },
+      {
+        role: "user",
+        content: `
+          Data: ${createdAt}
+          Refeição: ${text}
+        `,
+      },
+    ],
+  });
+
+  const json = response.choices[0].message.content;
+
+  if (!json) {
+    throw new Error("Failed to process meal.");
+  }
+
+  return JSON.parse(json);
 }
